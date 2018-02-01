b107b883122e366d13b8a558535832111d49aa6a
Aaron Lieberman <aaronmaxmusic@gmail.com>
2018-02-01 23:33:19 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
 
   def get_page
 
-    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 binding.pry
     # doc.css(".post").each do |post|
     #   course = Course.new
