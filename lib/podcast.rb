19b938c1349d4e0f9bcc9a19c91c8b30898f1c34
Aaron Lieberman <aaronmaxmusic@gmail.com>
2018-02-01 23:31:47 +0000

    automatically backed up by learn

---
+++ lib/podcast.rb
@@ -0,0 +1,22 @@
+class Podcast
+
+    attr_accessor :name, :position, :summary, :producer, :url
+
+    @@all = []
+
+    def initialize(name=nil, url=nil, producer=nil, position=nil)
+        @name = name
+        @url = url
+        @producer = producer
+        @position = position
+        @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+  def self.reset_all
+    @@all.clear
+  end
+end
