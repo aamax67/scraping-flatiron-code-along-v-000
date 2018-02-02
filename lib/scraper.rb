7014c1c40eec9187040a5074a34b54f9785cf705
Aaron Lieberman <aaronmaxmusic@gmail.com>
2018-02-02 06:10:06 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,31 +8,13 @@ class Scraper
 
     def get_page
       doc = Nokogiri::HTML(open("http://toppodcast.com/top-200-podcast/"))
-    end
 
-    def get_podcasts
-      self.get_page.css(".podcastRow")
-    end
-
-    def make_podcasts
-      self.get_podcasts.each do |p|
-      podcast = Podcast.new
-      podcast.name = p.css("h3").text.gsub(/\t/, '').strip
-      podcast.rank = p.css(".numberImage").text.gsub(/\t/, '').strip
-      podcast.summary = p.css("p").text.strip
+      doc.css(".podcastRow").each do |post|
+        podcast = Podcast.new
+        podcast.name = p.css("h3").text.gsub(/\t/, '').strip
+        podcast.rank = p.css(".numberImage").text.gsub(/\t/, '').strip
+        podcast.summary = p.css("p").text.strip
+        end
       end
     end
-
-
-  def print_podcasts
-    self.make_podcasts
-    Podcast.all.each do |podcast|
-      if podcast.name
-        puts "Name: #{podcast.name}"
-      end
-    end
-  end
-end
-
-
-  Scraper.new.print_podcasts
+binding.pry
